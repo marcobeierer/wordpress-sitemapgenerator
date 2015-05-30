db69f2e923cc0bf752c2e0d25e6b9012460d552d
marco <marco@webguerilla.net>
2015-05-30 19:18:18 +0200

    removed legacy code

--- src/sitemapgenerator.php
+++ src/sitemapgenerator.php
@@ -96,61 +96,6 @@ function sitemap_proxy_callback() {
 	wp_die();
 }
 
-/*function sitemap_generator_page(){
-
-	$PLG_AJAX_SITEMAPGENERATOR_SUCCESS="The generation of the sitemap was successfull. The sitemap was saved as sitemap.xml in the WordPress root folder.";
-	$PLG_AJAX_SITEMAPGENERATOR_ERROR="An error occurred. Please try it again or contact the developer of the extension.";
-	$PLG_AJAX_SITEMAPGENERATOR_ERROR_NOT_AUTHORISED="You are not authorised to generate a sitemap.";
-
-	$baseurl = get_site_url();
-	$baseurl64 = strtr(base64_encode($baseurl), '+/', '-_');
-
-	$subsequentRequest = false; // TODO implement a nicer solution
-	do {
-		if ($subsequentRequest) {
-			usleep(250000); // 250ms
-		} else {
-			$subsequentRequest = true;
-		}
-
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_URL, 'https://api.marcobeierer.com/sitemap/v2/' . $baseurl64 . '?pdfs=1&wordpress=1');
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-		$response = curl_exec($ch);
-
-		$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-		curl_close($ch);
-	}
-	while($statusCode == 200 && $contentType != 'application/xml');
-
-	if ($statusCode != 200) {
-		echo $PLG_AJAX_SITEMAPGENERATOR_ERROR; // TODO better error message
-		return;
-	}
-
-	$reader = new XMLReader();
-	$reader->xml($response, 'UTF-8');
-	$reader->setParserProperty(XMLReader::VALIDATE, true);
-
-	if ($reader->isValid()) { // TODO check if empty?
-
-		$rootPath = get_home_path();
-		if ($rootPath != '') {
-			file_put_contents($rootPath . DIRECTORY_SEPARATOR . 'sitemap.xml', $response); // TODO handle error
-			echo $PLG_AJAX_SITEMAPGENERATOR_SUCCESS;
-			return;
-		}
-	}
-
-	echo $PLG_AJAX_SITEMAPGENERATOR_ERROR; // TODO better error message
-	return;
-}*/
-
 add_action('admin_menu', 'register_sitemap_generator_settings_page');
 function register_sitemap_generator_settings_page() {
 	add_submenu_page('sitemap-generator', 'Sitemap Generator Settings', 'Settings', 'manage_options', 'sitemap-generator-settings', 'sitemap_generator_settings_page');
